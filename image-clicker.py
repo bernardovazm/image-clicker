2eb090f23f51eff65e242408682cd058b71636eb
Bernardo Vaz <77335626+bernardovazm@users.noreply.github.com>
2024-07-29 02:04:14 -0300

    Set globals in config.json file and improvements

--- image-clicker.py
+++ image-clicker.py
@@ -1,6 +1,10 @@
-import pyautogui, os, keyboard
+import pyautogui, os, keyboard, datetime, json
 images_path = "images"
 key_quit = "esc"
+file_extensions = [".png", ".jpg"]
+should_return_position = False
+grayscale = True
+duration = 0.1
 
 def create_folder(name):
     try:
@@ -9,6 +13,19 @@ def create_folder(name):
         return False
 create_folder(images_path)
 
+def load_config():
+    try:
+        globals_dict = globals()
+        with open("config.json", "r") as f:
+            config = json.load(f)
+            for var in config:
+                globals_dict[var] = config[var]
+            return(globals())
+    except FileNotFoundError:
+        print(f"File config.json not found, using default values...")
+    except Exception as e:
+        print(f"Exception: {e}")
+
 def find_image(image, grayscale=True):
     try:
         location = pyautogui.locateOnScreen(image, grayscale=grayscale)
@@ -16,22 +33,30 @@ def find_image(image, grayscale=True):
     except pyautogui.ImageNotFoundException:
         return False
 
-def click_image(path, duration=0.1, tween=pyautogui.easeInSine):
+def click_image(point, duration=duration, tween=pyautogui.easeInSine):
     try:
-        pyautogui.moveTo(path, duration=duration, tween=tween)
+        current_pos = pyautogui.position()
+        pyautogui.moveTo(point, duration=duration, tween=tween)
         pyautogui.click()
+        if should_return_position:
+            pyautogui.moveTo(current_pos, duration=duration, tween=tween)
+        print(f'{datetime.datetime.now()} - {str(point.replace(f"{os.getcwd()}/{images_path}/".replace("\\","/"), ""))} clicked')
     except:
         return False
 
 def main():
-    print(f"Searching for images to click, keep {key_quit} pressed to exit.")
+    print(f"{datetime.datetime.now()} - Script started, searching main monitor for images to click. Hold {key_quit} to exit.")
+    load_config()
     is_on = not keyboard.is_pressed(key_quit)
     while is_on:
         images_dir = os.listdir(f'{os.getcwd()}/{images_path}')
-        images_list = [str(f'{os.getcwd()}/{images_path}/{image}').replace("\\","/").replace(" ", " ") for image in images_dir]
+        images_list = [
+            str(f'{os.getcwd()}/{images_path}/{image}').replace("\\","/")
+            for image in images_dir
+            if image.endswith(tuple(file_extensions))
+            ]
         for image in images_list:
             if find_image(image) is not False:
-                click_image(image)
-                print(f'{image} clicked')
+                click_image(image, duration)
         is_on = not keyboard.is_pressed(key_quit)
 main()
