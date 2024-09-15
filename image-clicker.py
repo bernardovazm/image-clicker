3ac67e2e4ec5bdaccaa60c05e4bb683a132bb497
Bernardo Vaz <77335626+bernardovazm@users.noreply.github.com>
2024-09-15 16:32:53 -0300

    Add customizable confidence level

--- image-clicker.py
+++ image-clicker.py
@@ -1,10 +1,12 @@
-import pyautogui, os, keyboard, datetime, json
+import pyautogui, os, keyboard, datetime, json, cv2
+is_logger_active = False
 images_path = "images"
 key_quit = "esc"
 file_extensions = [".png", ".jpg"]
 should_return_position = False
 grayscale = True
 duration = 0.1
+confidence = 1.0
 
 def create_folder(name):
     try:
@@ -13,6 +15,9 @@ def create_folder(name):
         return False
 create_folder(images_path)
 
+def strip_image_path(image, images_path=images_path):
+    return str(image.replace(f"{os.getcwd()}/{images_path}/".replace("\\","/"), ""))
+
 def load_config():
     try:
         globals_dict = globals()
@@ -20,17 +25,25 @@ def load_config():
             config = json.load(f)
             for var in config:
                 globals_dict[var] = config[var]
+                if is_logger_active:
+                    print(f"G [{datetime.datetime.now()}] Variable {var} updated to: {globals_dict[var]}")
             return(globals())
     except FileNotFoundError:
         print(f"File config.json not found, using default values...")
     except Exception as e:
         print(f"Exception: {e}")
 
-def find_image(image, grayscale=True):
+def find_image(image, grayscale=True, confidence=1.0):
     try:
-        location = pyautogui.locateOnScreen(image, grayscale=grayscale)
+        location = pyautogui.locateOnScreen(image, grayscale=grayscale, confidence=confidence)
         return location
     except pyautogui.ImageNotFoundException:
+        if is_logger_active:
+            print(f"X [{datetime.datetime.now()}] {strip_image_path(image)} not found.")
+        return False
+    except Exception as e:
+        if is_logger_active:
+            print(f"X [{datetime.datetime.now()}] Except: {e}, {strip_image_path(image)}.")
         return False
 
 def click_image(point, duration=duration, tween=pyautogui.easeInSine):
@@ -40,12 +53,13 @@ def click_image(point, duration=duration, tween=pyautogui.easeInSine):
         pyautogui.click()
         if should_return_position:
             pyautogui.moveTo(current_pos, duration=duration, tween=tween)
-        print(f'{datetime.datetime.now()} - {str(point.replace(f"{os.getcwd()}/{images_path}/".replace("\\","/"), ""))} clicked')
+            if is_logger_active:
+                print(f"[{datetime.datetime.now()}] Mouse position returned.")
     except:
         return False
 
 def main():
-    print(f"{datetime.datetime.now()} - Script started, searching main monitor for images to click. Hold {key_quit} to exit.")
+    print(f"[{datetime.datetime.now()}] Script started, searching on your main monitor for images to click. Hold {key_quit} to exit.")
     load_config()
     is_on = not keyboard.is_pressed(key_quit)
     while is_on:
@@ -56,7 +70,9 @@ def main():
             if image.endswith(tuple(file_extensions))
             ]
         for image in images_list:
-            if find_image(image) is not False:
-                click_image(image, duration)
+            image_pos = find_image(image, grayscale, confidence)
+            if image_pos is not False:
+                click_image(image_pos, duration)
+                print(f"[{datetime.datetime.now()}] {strip_image_path(image)} clicked.")
         is_on = not keyboard.is_pressed(key_quit)
 main()
